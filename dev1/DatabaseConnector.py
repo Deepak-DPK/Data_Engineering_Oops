05c7955ed5c0a2e19ef0d947df0f1a0b51e93152
Deepak <deepakramesh1583@gmail.com>
2025-12-31 22:25:59 +0530

    encapsulation example

---
+++ dev1/DatabaseConnector.py
@@ -0,0 +1,20 @@
+class DatabaseConnector:
+    def __init__(self,host,user,password):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+    
+    def connect(self):
+        return f"Connected to {self.__host}"
+    
+    def get_host(self):
+        return self.__host
+    
+host=input("Enter the Host name : ")
+user=input("Enter the user name: ")
+password =input("Enter the password: ")
+
+d1=DatabaseConnector(host,user,password)
+
+d1.connect()
+d1.get_host()
