a43f9fd06f2c6bbb1aa346cdbff1853ff1f8d44e
Deepak <deepakramesh1583@gmail.com>
2026-01-01 18:49:12 +0530

    extra class added (paraquet)

--- dev2/LOAD_VALIDATE.py
+++ dev2/LOAD_VALIDATE.py
@@ -13,11 +13,18 @@ class CSVLoader(FileLoader):
     def validate(self):
         return "CSV schema Validated"
 
-class JSONLoader:
+class JSONLoader(FileLoader):
     def read(self):
         return "Reading json data"
     def validate(self):
         return "JSON structure is validated"
+    
+class ParaquetLoader(FileLoader):
+    def read(self):
+        return "Reading paraquet data"
+    def validate(self):
+        return "paraquet metadata is validated"
+
 
 def run_pipeline(loader : FileLoader):
     print(loader.read())
@@ -25,6 +32,8 @@ def run_pipeline(loader : FileLoader):
 
 csv=CSVLoader()
 Json=JSONLoader()
+paraquet=ParaquetLoader()
 
 run_pipeline(csv)
 run_pipeline(Json)
+run_pipeline(paraquet)
