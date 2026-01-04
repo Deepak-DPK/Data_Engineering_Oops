d2ed44058c7231f23423f4467283a245b1706d02
Deepak <deepakramesh1583@gmail.com>
2026-01-04 20:52:19 +0530

    all updated

--- structural/day1/pep8.py
+++ structural/day1/pep8.py
@@ -9,8 +9,6 @@ FileLoc = "/data/users.csv"
 MaxLimit = 100
 n = "John Doe"
  '''
-
-
 file_location = "/data/users.csv"  # snake_case for variables
 MAX_LIMIT = 100                    # ALL_CAPS for constants
 user_name = "John Doe"             # Descriptive snake_case
@@ -22,17 +20,17 @@ you assume it's a blueprint (Class).
 When you see file_location, you know it's data (Variable).
 """
 
-
 """
-Q2. Data Cleaning You have a raw price string from a website: raw_price = "$$1,200.50" Write code to:
+Q2. Data Cleaning 
+You have a raw price string from a website: raw_price = "$$1,200.50" Write code to:
 
 Strip the $$.
 
-Slice out just the 200.50 (ignoring the 1, for now to keep it simple, or handle it if you know how). Hint: Just slice from the correct index.
+Slice out just the 200.50 
+(ignoring the 1, for now to keep it simple, or handle it if you know how). 
+Hint: Just slice from the correct index.
 
 """
-
-
 
 raw_price = "$$1,200.50"
 
@@ -42,10 +40,9 @@ new_price = whole_price[2:]
 
 print(new_price)
 
-
-
 """
-Q3. f-String Logic Create a variable table = "users". Write an f-string that prints: "SELECT * FROM users WHERE active = 1;"
+Q3. f-String Logic Create a variable table = "users". 
+Write an f-string that prints: "SELECT * FROM users WHERE active = 1;"
 """
 
 table = "users"
@@ -57,7 +54,9 @@ print(f"SELECT * FROM {table} WHERE active = 1;")
 
 """
 Day 1 Mini-Exam (3 Questions)
-Q1. Debugging & PEP8 The following code crashes and looks unprofessional. Rewrite it using strict PEP8 and fix the logic error (trying to add string to integer).
+Q1. Debugging & PEP8 The following code crashes and looks unprofessional. 
+Rewrite it using strict PEP8 and fix the logic error 
+(trying to add string to integer).
 
 Python
 
